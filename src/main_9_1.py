0106a84ee688fdac908e269c8708a8f82fe4dae1
Timofeev Artem <artemtimofeev@rambler.ru>
2024-06-03 20:04:29 +0300

    DZ_hard project was created

---
+++ src/main_9_1.py
@@ -0,0 +1,34 @@
+# Импортируем необходимые библиотеки
+import os
+from typing import Optional
+
+
+def directory_info(directory: Optional[str] = None, recurs_count: bool = False) -> dict:
+    """Функция возвращает словарь с указанием количества файлов и папок в указанноу директории.
+    Если директория не указана, по-умолчанию принимается директория нахождения текущего файла.
+    Если в необязательном параметре 'recurs_count' передано значение 'True',
+    выполняется рекурсивный подсчёт файлов и папок, включая вложенные,
+    по-умолчанию значение необязательного параметра - 'False'"""
+    # Определяем переменные для количества файлов и папок.
+    files_count = 0
+    folders_count = 0
+    # В зависимости от переданного параметра директории определяем путь: указанный, либо текущий.
+    if directory and os.path.isdir(directory):
+        path = directory
+    else:
+        path = os.getcwd()
+    # В зависимости от значения параметра 'recurs_count' ведём поверхностный (применяем os.csandir())
+    # либо рекурсивный (применяем os.walk()) подсчёт файлов и папок.
+    if not recurs_count:
+        with os.scandir(path) as dir_info:
+            for item in dir_info:
+                if item.is_file():
+                    files_count += 1
+                elif item.is_dir():
+                    folders_count += 1
+    else:
+        for _, folders, files in list(os.walk(path)):
+            files_count += len(files)
+            folders_count += len(folders)
+    # Возвращаем результат в виде словаря заданного вида.
+    return {"files": files_count, "folders": folders_count}
---
+++ src/main_9_2.py
@@ -0,0 +1,12 @@
+def strings_a_a(strings: list[str]) -> list[str]:
+    """Функция принимает список строк и возвращает список строк, у которых первая и последняя буквы одинаковые"""
+    return [word for word in strings if word == "" or word[0] == word[-1]]
+
+
+def max_multy(nums: list[int]) -> int:
+    """Функция принимает список целых чисел и возвращает максимальное произведение двух из переданных чисел.
+    Если в списке меньше 2 чисел, возвращает 0."""
+    if len(nums) >= 2:
+        return max([nums[i] * nums[j] for i in range(len(nums) - 1) for j in range(1, len(nums))])
+    else:
+        return 0
